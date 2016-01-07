b3c349d40e409c71e4ffefd250ddc41cc73f129d
Stefano Lacaprara <stefano.lacaprara@pd.infn.it>
2016-01-07 13:57:52 +0100

    fix for background

--- steering_files/submit_lsf.sh
+++ steering_files/submit_lsf.sh
@@ -1,27 +1,43 @@
 #!/bin/bash
 
 nFilesPerJob=10
+nMaxFiles=10
+
+# what='signal'
+what='uubar'
+# what='ddbar'
+# what='ssbar'
+# what='ccbar'
 
 channelName=unknown
 setChannelName() {
   ch=$1
+  what=$2
   case $ch in
-    [1]*)
+    1)
       channelName=B0_etapr-eta-gg2pi_KS-pi+pi-
     ;;
-    [2]*)
+    2)
       channelName=B0_etapr-eta-gg2pi_KS-pi0pi0
     ;;
-    [3]*)
+    3)
     ;;
-    [4]*)
+    4)
       channelName=B0_etapr-eta-3pi2pi_KS-pi+pi-
     ;;
-    [5]*)
+    5)
       channelName=B0_etapr-eta-3pi2pi_KS-pi0pi0
     ;;
-    [6]*)
+    6)
     ;;
+  esac
+  case $what in
+    'signal')
+      fileList=${channelName}_gsim-BKGx0.list
+      ;;
+    *)
+      fileList=Background_${what}_BGx1.list
+      ;;
   esac
 }
 
@@ -30,7 +46,9 @@ payload() {
   channel=$2
   firstFile=$3
   numFiles=$4
-  payloadName=ch${channel}_${job}
+  what=$5
+
+  payloadName=ch${channel}_${job}_${what}
 
 cat > payload_${payloadName}.sh << EOF
 #!/bin/sh
@@ -46,8 +64,8 @@ pwd
 mkdir payload_${payloadName}/
 cd payload_${payloadName}/
 cp ../SelectEtaPrK0_ch${channel}.py .
-cp ../${channelName}_gsim-BKGx0.list .
-time basf2 SelectEtaPrK0_ch${channel}.py $numFiles $firstFile
+cp ../${fileList} .
+time basf2 SelectEtaPrK0_ch${channel}.py $numFiles $firstFile $what
 mv ${channelName}_output_signal.root ${channelName}_output_signal_${job}.root
 
 date
@@ -56,16 +74,17 @@ EOF
 }
 
 #for channel in {1,2,4,5}
-for channel in {2,4,5}
+for channel in 1
 do
   job=0
-  setChannelName $channel
-  nFiles=$(wc -l ${channelName}_gsim-BKGx0.list | awk '{print $1}' )
+  setChannelName $channel $what
+  nFiles=$(wc -l ${fileList} | awk '{print $1}' )
+  if [[ $nFiles -gt $nMaxFiles ]] ; then nFiles=$nMaxFiles; fi
   echo echo "Submitting $(echo $nFiles*1./$nFilesPerJob| bc) jobs over $((nFiles)) files for channel $channel $channelName"
-  for files in `seq 0 $nFilesPerJob $nFiles`
-  do
-      payload $job $channel $files $nFilesPerJob
-      bsub < payload_${payloadName}.sh
+  for firstFile in `seq 0 $nFilesPerJob $(($nFiles-1))`
+  do  
+      payload $job $channel $firstFile $nFilesPerJob $what
+      #bsub < payload_${payloadName}.sh
       ((job++))
   done
   echo "done"
