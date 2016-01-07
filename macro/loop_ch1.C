59352c31981519b9a082a186ee9b99d83cc7f1e1
Stefano Lacaprara <stefano.lacaprara@pd.infn.it>
2016-01-07 18:28:37 +0100

    multiple input

--- macro/loop_ch1.C
+++ macro/loop_ch1.C
@@ -4,7 +4,8 @@
 
 void loop_ch1(int nev=10000) {
 
-  TFile *_file0 = TFile::Open("../root_files/B0_etapr-eta-gg2pi_KS-pi+pi-_output_signal.root");
+  //TFile *_file0 = TFile::Open("../root_files/ch1/B0_etapr-eta-gg2pi_KS-pi+pi-_output_signal.root");
+  TFile *_file0 = TFile::Open("../root_files/ch1/uubar/B0_etapr-eta-gg2pi_KS-pi+pi-_output_signal_0.root");
 
   TTree* B0t=(TTree*) _file0->Get("B0"); 
   B0_ch1 B0(B0t);
